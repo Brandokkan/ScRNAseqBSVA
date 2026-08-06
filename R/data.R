fac4af092eb285862fa0007cce9393cb75034bfb
brando <brandokkan@gmail.com>
2026-08-07 00:24:51 +0200

    Added raw single-cell in multiple formats data and script to generate SeuratObject from the raw data

---
+++ R/data.R
@@ -0,0 +1,7 @@
+#' Example single-cell object
+#'
+#' A subsetted Seurat object used in examples and tests.
+#'
+#' @format A `Seurat` object with 78 cells and 9337 features.
+#' @source Derived from Panglao dataset.
+"so_obj"
---
+++ data-raw/create_SO.R
@@ -0,0 +1,8 @@
+## code to prepare `SeuratObject` dataset goes here
+mat_path <- system.file("extdata", "sc_test.mtx", package = "ScRNAseqBSVA")
+gene_path <- system.file("extdata", "genes.tsv", package = "ScRNAseqBSVA")
+cell_path <- system.file("extdata", "barcode.tsv", package = "ScRNAseqBSVA")
+
+so_obj <- Seurat::ReadMtx(mat_path, cell_path, gene_path, feature.column = 1)
+
+usethis::use_data(so_obj, overwrite = TRUE, compress = "xz")
