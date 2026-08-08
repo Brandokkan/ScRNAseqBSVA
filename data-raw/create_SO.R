e423fd218476d2bc91ed4e56086aecc268ddbdc6
brando <brandokkan@gmail.com>
2026-08-08 19:38:11 +0200

    Fixed create_SO.R to actually output a SeuratObject instead of a large dgCMatrix

--- data-raw/create_SO.R
+++ data-raw/create_SO.R
@@ -4,5 +4,6 @@ gene_path <- system.file("extdata", "genes.tsv", package = "ScRNAseqBSVA")
 cell_path <- system.file("extdata", "barcode.tsv", package = "ScRNAseqBSVA")
 
 so_obj <- Seurat::ReadMtx(mat_path, cell_path, gene_path, feature.column = 1)
+so_obj <- Seurat::CreateSeuratObject(so_obj)
 
 usethis::use_data(so_obj, overwrite = TRUE, compress = "xz")
