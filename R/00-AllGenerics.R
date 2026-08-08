48b51a619a4d666f8c88a2b7374badddbe829753
brando <brandokkan@gmail.com>
2026-08-08 23:28:31 +0200

    Expanded the description of the package and added the read_sc_file function

--- R/00-AllGenerics.R
+++ R/00-AllGenerics.R
@@ -18,3 +18,29 @@
 setGeneric("convert_to_so", function(sc_data, ...) {
   standardGeneric("convert_to_so")
 })
+
+
+#' Reads one or multiple single cell files and outputs
+#' the corresponding variable containing the information
+#' inside the file(s)
+#'
+#' The output variable can be used for single-cell analysis
+#'
+#' @param path the path of the file containing the read counts
+#' @param ... additional parameters
+#'
+#' @return a variable containing the information in the file(s)
+#'  in the corresponding format recognized formats
+#'
+#' @export
+#' @examples
+#' mat_path <- system.file("extdata", "sc_test.mtx", package = "ScRNAseqBSVA")
+#' gene_path <- system.file("extdata", "genes.tsv", package = "ScRNAseqBSVA")
+#' cell_path <- system.file("extdata", "barcode.tsv", package = "ScRNAseqBSVA")
+#' read_sc_file(mat_path, gene_path, cell_path)
+#'
+#' csv_path <- system.file("extdata", "sc_test.csv", package = "ScRNAseqBSVA")
+#' read_sc_file(csv_path, header = FALSE)
+setGeneric("read_sc_file", function(path, ...) {
+  standardGeneric("read_sc_file")
+})
---
+++ R/read_sc_file.R
@@ -0,0 +1,19 @@
+#' @describeIn read_sc_file Method for creating the corresponding single-cell
+#'  variable from a file or multiple files. For .mtx files, a gene and cell
+#'  barcodes must also be submitted in .tsv format
+#' @param bar_path the path of the .tsv file containing the cells bar-codes
+#' @param gene_path the path of the .tsv file containing the names of the genes
+#' @importFrom Seurat ReadMtx
+#' @export
+setMethod("read_sc_file", signature(path = "character"), function(path, bar_path, gene_path,
+                                                                  ...) {
+  if (grepl(".mtx$", path)) {
+    ReadMtx(path, bar_path, gene_path, feature.column = 1, ...)
+  } else if (grepl(".csv$", path)) {
+    read.csv(path, ...)
+  } else if (grepl(".tsv$", path)) {
+    read.csv(path, sep = "\t", ...)
+  } else {
+    stop("type of file not recognized or supported", call. = FALSE)
+  }
+})
