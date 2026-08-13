3774b838e2980c05d2c56094b54340aa81121319
brando <brandokkan@gmail.com>
2026-08-13 17:29:35 +0200

    Added documentation for so_no_ens and so_mit_rbp

--- R/data.R
+++ R/data.R
@@ -5,3 +5,22 @@
 #' @format A `Seurat` object with 78 cells and 9337 features.
 #' @source Derived from Panglao dataset.
 "so_obj"
+
+#' Example single-cell object without the ENSEMBL suffix
+#'
+#' A subsetted Seurat object used in examples and tests. The ENSEBLE suffix was
+#' removed.
+#'
+#' @format A `Seurat` object with 78 cells and 9337 features.
+#' @source Derived from Panglao dataset.
+"so_no_ens"
+
+#' Example single-cell object with percentage of mitochondrial and ribosomial
+#' genes calculated.
+#'
+#' A subsetted Seurat object used in examples and tests. The percentage of
+#' ribosomal and mitochondrial genes are already calculated for each cell.
+#'
+#' @format A `Seurat` object with 78 cells and 9337 features.
+#' @source Derived from Panglao dataset.
+"so_mit_rbp"
