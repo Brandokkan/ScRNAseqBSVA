a54e9b38a5a09b47fea836d3a3d483dbc50db2dd
Brandokkan <brandokkan@gmail.com>
2026-08-20 04:13:20 +0200

    Added suggested packages needed for test-sc_deconvolute, improved and added method for SeuratObjects in sc_deconvolute and related test.

--- R/sc_deconvolute.R
+++ R/sc_deconvolute.R
@@ -2,6 +2,8 @@
 #' from a reference SummarizedExperiment dataset
 #' @importFrom Seurat as.SingleCellExperiment
 #' @importFrom SummarizedExperiment assayNames
+#' @importFrom SummarizedExperiment colData
+#' @importFrom SummarizedExperiment colnames
 #' @importFrom SingleR SingleR
 #' @export
 setMethod("sc_deconvolute", signature(so = "Seurat", ref = "SummarizedExperiment"),
@@ -9,8 +11,14 @@ setMethod("sc_deconvolute", signature(so = "Seurat", ref = "SummarizedExperiment
   sce <- as.SingleCellExperiment(so)
 
   if (!"logcounts" %in% assayNames(sce)) {
-    stop("Error: logcounts is not present in the SingleCellExperiment. Make sure
-         the SeuratObject was LogNormalized", call. = FALSE )
+    stop("Error: logcounts is not present in the query. Make sure
+         the query was LogNormalized", call. = FALSE )
+  }
+
+  if (!lab_name %in% colnames(colData(ref))) {
+    stop("Error: lab_name value is not in colData. Try looking in colData of ref
+         and see what is the name of the vector containing the cell labels",
+         call. = FALSE)
   }
 
   predictions <- SingleR(
@@ -33,7 +41,6 @@ setMethod("sc_deconvolute", signature(so = "Seurat", ref = "SummarizedExperiment
 #' @describeIn sc_deconvolute method used to predict cell types in a
 #' SummarizedExperiment or SingleCellExperiment from a reference
 #' SummarizedExperiment or SingleCellExperiment dataset
-#' @importFrom Seurat as.SingleCellExperiment
 #' @importFrom SummarizedExperiment assayNames
 #' @importFrom SingleR SingleR
 #' @importFrom SummarizedExperiment colData
@@ -44,8 +51,8 @@ setMethod("sc_deconvolute", signature(so = "SummarizedExperiment", ref = "Summar
   sce <- so
 
   if (!"logcounts" %in% assayNames(sce)) {
-    stop("Error: logcounts is not present in the SingleCellExperiment. Make sure
-          the SeuratObject was LogNormalized", call. = FALSE )
+    stop("Error: logcounts is not present in the query. Make sure
+          the query was LogNormalized", call. = FALSE )
   }
 
   if (!lab_name %in% colnames(colData(ref))) {
@@ -69,3 +76,38 @@ setMethod("sc_deconvolute", signature(so = "SummarizedExperiment", ref = "Summar
 
   so
 })
+
+
+#' @describeIn sc_deconvolute method used to predict cell types in a
+#' SeuratObject from a reference SeuratObject
+#' @importFrom SeuratObject Layers
+#' @importFrom Seurat FindTransferAnchors
+#' @importFrom Seurat TransferData
+#' @importFrom Seurat AddMetaData
+#' @export
+setMethod("sc_deconvolute", signature(so = "Seurat", ref = "Seurat"),
+          function(so, ref, diagnosis = TRUE, lab_name = "seurat_annotations") {
+
+            if (!"data" %in% Layers(so[["RNA"]])) {
+              stop("Error: data is not present in the query SeuratObject. Make sure
+                   the SeuratObject was LogNormalized", call. = FALSE )
+            }
+
+            if (!lab_name %in% colnames(ref@meta.data)) {
+              stop("Error: lab_name value is not in meta.data. Try looking in meta.data of ref
+                    and see what is the name of the vector containing the cell labels",
+                   call. = FALSE)
+            }
+
+            anchors <- FindTransferAnchors(reference = ref,
+                                           query = so)
+
+            predictions <- TransferData(anchorset = anchors, refdata = ref[[lab_name, drop = TRUE]])
+            so <- AddMetaData(object = so, metadata = predictions)
+
+            if (diagnosis) {
+              prediction_diagnostics(so)
+            }
+
+            so
+          })
--- tests/testthat/test-sc_deconvolute.R
+++ tests/testthat/test-sc_deconvolute.R
@@ -1,5 +1,9 @@
 data("so_norm_log")
 data("so_mit_rbp")
+pbmc3k <- SeuratData::LoadData("pbmc3k")
+pbmc3k <- Seurat::UpdateSeuratObject(pbmc3k)
+pbmc3k <- sc_normalization(pbmc3k)
+pbmc3k <- sc_clustering(pbmc3k)
 suex_dataset <- celldex::MouseRNAseqData()
 # so_suex <- Seurat::as.Seurat(as(suex_dataset, "SingleCellExperiment"), counts = NULL)
 scex <- scRNAseq::ZeiselBrainData()
@@ -19,3 +23,9 @@ test_that("method for SummarizedExperiment and SingleCellExperiment works and gi
   expect_gt(concordance, 0.85)
 })
 
+test_that("method for SeuratObject works and gives sound results", {
+  pbm_labels <- pbmc3k@meta.data$seurat_annotations
+  pbm_labels_p <- sc_deconvolute(pbmc3k, pbmc3k, diagnosis = FALSE)@meta.data$predicted.id
+  concordance <- mean(pbm_labels == pbm_labels_p, na.rm = TRUE)
+  expect_gt(concordance, 0.85)
+})
